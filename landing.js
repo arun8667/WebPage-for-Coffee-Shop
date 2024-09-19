581f098b2df318bc77fb6969a85bba2e85294268
Arun <mailtoarun2002@gmail.com>
2024-09-19 21:15:09 +0530

    first commit

---
+++ landing.js
@@ -0,0 +1,26 @@
+function booking()
+    {
+        var fname = document.getElementById('FirstName').value;
+        var name1 = document.getElementById('name').value;
+        var number1 = document.getElementById('number').value;
+        var booking_date1 = document.getElementById('booking_date').value;
+        
+
+
+    
+
+    if(fname&&name&&number1&&booking_date1!=="")
+    {
+        alert('submitted successfuly')
+    }
+    else{
+        alert('Enter the Details')
+    }
+
+
+    function mapping() {
+
+        
+    }
+    
+}
